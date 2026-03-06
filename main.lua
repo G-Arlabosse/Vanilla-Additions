b16e152f5290562a158d7dfe325b00bd3e72764a
Gillian Arlabosse <62660428+G-Arlabosse@users.noreply.github.com>
2026-03-06 15:47:32 +0100

    Mod setup
    Organizing files

---
+++ main.lua
@@ -0,0 +1,32 @@
+Mod = RegisterMod("Vanilla Additions", 1)
+local damagePotion = Isaac.GetItemIdByName("Damage Potion")
+local damagePotionDamage = 1
+
+function Mod:EvaluateCache(player, cacheFlags)
+    if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
+        local itemCount = player:GetCollectibleNum(damagePotion)
+        local damageToAdd = damagePotionDamage * itemCount
+        player.Damage = player.Damage + damageToAdd
+    end
+end
+
+Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Mod.EvaluateCache)
+
+local bigRedButton = Isaac.GetItemIdByName("Big Red Button")
+
+function Mod:RedButtonUse(item)
+    local roomEntities = Isaac.GetRoomEntities()
+    for _, entity in ipairs(roomEntities) do
+        if entity:IsActiveEnemy() and entity:IsVulnerableEnemy() then
+            entity:Kill()
+        end
+    end
+
+    return {
+        Discharge = true,
+        Remove = false,
+        ShowAnim = true
+    }
+end
+
+Mod:AddCallback(ModCallbacks.MC_USE_ITEM, Mod.RedButtonUse, bigRedButton)
