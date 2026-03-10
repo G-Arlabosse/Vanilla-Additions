6e8bf3afeba44c93488e0ef337fcdcd8ae99bdf1
leokoko42 <leokocik0@gmail.com>
2026-03-10 21:21:06 +0100

    Fixed Non Shop Room Rerolls
    Rerolling in a room that is non-shop now works
    But dropping an active item also updates the cost

--- scripts/items/fallen_angel.lua
+++ scripts/items/fallen_angel.lua
@@ -15,21 +15,30 @@ end
 function Mod:CheckPedestals(
     pickup ---@param pickup EntityPickup
 )
+    print("=== PICKUP ===")
     local roomDesc = Game():GetLevel():GetCurrentRoomDesc().Data
     if (roomDesc.Type == RoomType.ROOM_ANGEL) or (true) then
         --print(pickup.Variant, variant)
         if pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+            local item_config = item_config:GetCollectible(pickup.SubType)
+
             if pickup.Price >= 0 then
-                local item_config = item_config:GetCollectible(pickup.SubType)
 
                 pickup.AutoUpdatePrice = false
                 pickup.Price = -item_config.DevilPrice
 
                 print("Price set to 1 heart")
-                print("Name:" .. item_config.Name .. 
-                ", DevilPrice:" .. item_config.DevilPrice ..
-                ", ActualPrice:" .. pickup.Price)
             end
+            print(
+                "Variant:" .. pickup.Variant ..
+                ", Name:" .. item_config.Name .. 
+                ", Id:" .. pickup.SubType ..
+                ", DevilPrice:" .. item_config.DevilPrice ..
+                ", ActualPrice:" .. pickup.Price
+            )
+            print(pickup:IsShopItem())
+        else
+            print("Variant: " .. pickup.Variant)
         end
         --local pedestals = GetPedestalsInRoom()
         --print(#pedestals)
@@ -61,7 +70,7 @@ function Mod:PickupUpdate(
     if not data.DevilPriced then
         if pickup.Price == 0 and not pickup:IsShopItem() then
             pickup.AutoUpdatePrice = false
-            pickup.Price = PickupPrice.PRICE_ONE_HEART
+            pickup.Price = -1
         end
 
         data.DevilPriced = true
@@ -70,16 +79,68 @@ end
 
 --Mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, Mod.PickupUpdate)
 
-function Mod:PrePickupMorph(pickup, entityType, variant, subtype)
-    if variant == PickupVariant.PICKUP_COLLECTIBLE then
-        pickup.AutoUpdatePrice = false
-        pickup.Price = PickupPrice.PRICE_ONE_HEART
-        return true
+function Mod:PrePickupMorph(
+    pickup,     ---@param pickup EntityPickup
+    entityType, ---@param entityType EntityType
+    variant,    ---@param variant PickupVariant
+    subtype
+)
+    print("=== PRE MORPH ===")
+    if entityType == EntityType.ENTITY_PICKUP and pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+        print("Yes")
+        local config = Isaac.GetItemConfig():GetCollectible(pickup.SubType)
+        if config then
+
+            -- Set Devil price
+            pickup.Price = 0
+            print(
+                "EntityType:" .. entityType ..
+                ", Variant" .. pickup.Variant ..
+                ", Name:" .. config.Name .. 
+                ", Id:" .. pickup.SubType ..
+                ", DevilPrice:" .. config.DevilPrice ..
+                ", ActualPrice:" .. pickup.Price)
+            print(pickup:IsShopItem())
+            
+        end
     else
+        print("Nope")
         return false
     end
 end
 
-Mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_MORPH, Mod.PrePickupMorph)
+function Mod:PostPickupMorph(
+    pickup,     ---@param pickup EntityPickup
+    entityType, ---@param entityType EntityType
+    variant    ---@param variant PickupVariant
+)
+    print("=== POST MORPH ===")
+    if entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_COLLECTIBLE then
+        print("Okay")
+        --pickup.Price = PickupPrice.PRICE_FREE
+        local config = Isaac.GetItemConfig():GetCollectible(pickup.SubType)
+        if config then
+            pickup.Price = -config.DevilPrice
+            --pickup.AutoUpdatePrice = false
+            -- Set Devil price
+            print(
+                "EntityType:" .. entityType ..
+                ", Variant" .. pickup.Variant ..
+                ", Name:" .. config.Name .. 
+                ", Id:" .. pickup.SubType ..
+                ", DevilPrice:" .. config.DevilPrice ..
+                ", ActualPrice:" .. pickup.Price)
+            print(pickup:IsShopItem())
+        end
+    else
+        print("NotOkay")
+    end
+    
+end
 
-Mod:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, Mod.CheckPedestals, PickupVariant.PICKUP_COLLECTIBLE)
+
+Mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_MORPH, Mod.PrePickupMorph)
+Mod:AddCallback(ModCallbacks.MC_POST_PICKUP_MORPH, Mod.PostPickupMorph)
+
+Mod:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, Mod.CheckPedestals)--, PickupVariant.PICKUP_COLLECTIBLE)
+-- Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, Mod.CheckPedestals)
