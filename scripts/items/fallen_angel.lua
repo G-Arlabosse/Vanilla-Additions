a101ed7f72f07d1134ca3dc23113d0d82203a34e
leokoko42 <leokocik0@gmail.com>
2026-03-11 22:15:39 +0100

    Pedestal Reroll
    Pedestal rerolls do not turn back into heart priced items

--- scripts/items/fallen_angel.lua
+++ scripts/items/fallen_angel.lua
@@ -1,12 +1,24 @@
 local item_config = Isaac.GetItemConfig()
 local pending_morhped_items = {}
+local devil_pedestals = {}
+local fallen_angel = Isaac.GetItemIdByName("Fallen Angel")
+
+local function FallenAngelActive ()
+    local roomDesc = Game():GetLevel():GetCurrentRoomDesc().Data
+    if not (roomDesc.Type == RoomType.ROOM_ANGEL) then return false end
+    for i=0, Game():GetNumPlayers() -1 do
+        local player = Isaac.GetPlayer(i)
+        if player:GetCollectibleNum(fallen_angel) > 0 then
+            return true
+        end
+    end
+    return false
+end
 
 function Mod:InitPedestals(
     pickup ---@param pickup EntityPickup
 )
-    local roomDesc = Game():GetLevel():GetCurrentRoomDesc().Data
-    if (roomDesc.Type == RoomType.ROOM_ANGEL) or (true) then
-        --print(pickup.Variant, variant)
+    if FallenAngelActive() then
         if pickup.Variant == PickupVariant.PICKUP_COLLECTIBLE then
             local item_config = item_config:GetCollectible(pickup.SubType)
             pickup.AutoUpdatePrice = false
@@ -28,6 +40,8 @@ end
 
 
 function Mod:PostUpdate() 
+    if not FallenAngelActive() then return end
+
     if #pending_morhped_items > 0 then
         if not ActiveItemPickedUp() then
             for _,pickup in pairs(pending_morhped_items) do
@@ -42,6 +56,7 @@ function Mod:PostUpdate()
     end
 end
 
+local morphed_item_devil = false
 
 function Mod:PrePickupMorph(
     pickup,     ---@param pickup EntityPickup
@@ -49,6 +64,12 @@ function Mod:PrePickupMorph(
     variant,    ---@param variant PickupVariant
     subtype
 )
+    if not FallenAngelActive() then return end
+    print("=== Pre Morph ===")
+    print("Item: ", pickup.SubType, "ShopId: ", pickup.Index)
+    if pickup.Price < 0 then
+        morphed_item_devil = true
+    end
     pickup:GetData().priceReset = true
     pickup.Price = 0
     
@@ -60,9 +81,11 @@ function Mod:PostPickupMorph(
     entityType, ---@param entityType EntityType
     variant    ---@param variant PickupVariant
 )
-    if entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_COLLECTIBLE then
+    if not FallenAngelActive() then return end
+    print("=== POST MORPH ===")
+    if entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_COLLECTIBLE and morphed_item_devil then
         pending_morhped_items[#pending_morhped_items + 1] = pickup
-        print(#pending_morhped_items)
+        morphed_item_devil = false
     end
     
 end
