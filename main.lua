099c6a83775e776a4c4783847daba4e456e57cf8
leokoko42 <leokocik0@gmail.com>
2026-03-15 16:32:34 +0100

    Init Item

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@ include("scripts.items.bell_baby")
 include("scripts.items.fallen_angel")
 include("scripts.items.life_orb")
 include("scripts.items.corrupted_clover")
+include("scripts.items.secrets_of_the_lost")
 
 local damagePotion = Isaac.GetItemIdByName("Damage Potion")
 local damagePotionDamage = 1
