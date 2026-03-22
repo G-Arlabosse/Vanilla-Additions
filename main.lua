789f0a2d092ce26f2d4df605a7f9c4cef4e862cd
leokoko42 <leokocik0@gmail.com>
2026-03-22 16:23:30 +0100

    Init Cursed D6

--- main.lua
+++ main.lua
@@ -15,6 +15,7 @@ include("scripts.items.fallen_angel")
 include("scripts.items.life_orb")
 include("scripts.items.corrupted_clover")
 include("scripts.items.cursed_map")
+include("scripts.items.cursed_d6")
 
 local damagePotion = Isaac.GetItemIdByName("Damage Potion")
 local damagePotionDamage = 1
