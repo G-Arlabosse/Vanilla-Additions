3bb59d204db4fd8fe3e7efd23ee22b5bf42b60e1
leokoko42 <leokocik0@gmail.com>
2026-03-27 16:02:45 +0100

    Cursed Floors Init

--- main.lua
+++ main.lua
@@ -15,6 +15,7 @@ include("scripts.items.fallen_angel")
 include("scripts.items.life_orb")
 include("scripts.items.corrupted_clover")
 include("scripts.items.cursed_map")
+include("scripts.items.cursed_floors")
 
 local damagePotion = Isaac.GetItemIdByName("Damage Potion")
 local damagePotionDamage = 1
