ae57e2ddba7e98ad7c7514b22b4c8bd16d32ddd1
leokoko42 <leokocik0@gmail.com>
2026-03-29 16:47:21 +0200

    Fixed synergy with Soy/Almond and tear color
    - Gave TearSoy color
    - Refresh the multipliers after picking up soy/almond milks

--- scripts/items/calcium.lua
+++ scripts/items/calcium.lua
@@ -1,6 +1,8 @@
 local calcium = Isaac.GetItemIdByName("Calcium")
 local DAMAGE_MULTIPLIER = 0.25
 local FIRE_RATE_MULTIPLIER = 5.5
+local TEAR_SCALE = 0.4
+local TEAR_KNOCKBACK = 0.2
 
 local function toTearsPerSecond(maxFireDelay)
   return 30 / (maxFireDelay + 1)
@@ -16,17 +18,23 @@ local function calciumUse(_, item, rng, player)
     local data = player:GetData()
 
     --- ALMOND_MILK ---
-    if player_weapon_modifiers & WeaponModifier.ALMOND_MILK > 0 then
+    if player:HasCollectible(CollectibleType.COLLECTIBLE_ALMOND_MILK) then
         data.opikoko_calcium_damage_mult = DAMAGE_MULTIPLIER/0.3
         data.opikoko_calcium_fire_rate_mult = FIRE_RATE_MULTIPLIER/4
+        data.opikoko_calcium_tear_scale = 1
+        data.opikoko_calcium_tear_knockback = 1
     --- SOY_MILK ---
-    elseif player_weapon_modifiers & WeaponModifier.SOY_MILK > 0 then
+    elseif player:HasCollectible(CollectibleType.COLLECTIBLE_SOY_MILK) then
         data.opikoko_calcium_damage_mult = DAMAGE_MULTIPLIER/0.2
         data.opikoko_calcium_fire_rate_mult = FIRE_RATE_MULTIPLIER/5.5
+        data.opikoko_calcium_tear_scale = 1
+        data.opikoko_calcium_tear_knockback = 1
 
     else
         data.opikoko_calcium_damage_mult = DAMAGE_MULTIPLIER
         data.opikoko_calcium_fire_rate_mult = FIRE_RATE_MULTIPLIER
+        data.opikoko_calcium_tear_scale = TEAR_SCALE
+        data.opikoko_calcium_tear_knockback = TEAR_KNOCKBACK
     end
 
     data.opikoko_calcium_active = true
@@ -67,7 +75,7 @@ local function evaluateCache(_, player, cacheFlags)
         
             -- Update Tear color
         elseif cacheFlags == CacheFlag.CACHE_TEARCOLOR then
-            player.TearColor = Color(1,1,1,1,0.5,0.5,0.5)
+            player.TearColor = Color.TearSoy
         end
     end
 end
@@ -76,8 +84,8 @@ local function changeTearProperties(_, tear)
     local player = tear.SpawnerEntity
     local data = player:GetData()
     if data.opikoko_calcium_active then
-        tear.Scale = tear.Scale * 0.4
-        tear.KnockbackMultiplier = tear.KnockbackMultiplier * 0.2
+        tear.Scale = tear.Scale * data.opikoko_calcium_tear_scale
+        tear.KnockbackMultiplier = tear.KnockbackMultiplier * data.opikoko_calcium_tear_knockback
     end
 end
 
@@ -92,6 +100,12 @@ local function PostWeaponFire(_,
     end
 end
 
+local function PostAddCollectible(_,type,charge,firstTime,slot,varData,player)
+    if type == CollectibleType.COLLECTIBLE_SOY_MILK or type == CollectibleType.COLLECTIBLE_ALMOND_MILK then
+        calciumUse(_, calcium, RNG(), player)
+    end
+end
+
 Mod:AddCallback(ModCallbacks.MC_USE_ITEM, calciumUse, calcium)
 Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, calciumDeactivate)
 Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, evaluateCache, CacheFlag.CACHE_DAMAGE)
@@ -99,3 +113,4 @@ Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, evaluateCache, CacheFlag.CACHE_F
 Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, evaluateCache, CacheFlag.CACHE_TEARCOLOR)
 Mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, changeTearProperties)
 Mod:AddCallback(ModCallbacks.MC_POST_WEAPON_FIRE, PostWeaponFire)
+Mod:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, PostAddCollectible)
