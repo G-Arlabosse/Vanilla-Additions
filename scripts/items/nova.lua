1b7318b8cfd3f5f6c219a7261fe2a85cf10bd94c
leokoko42 <leokocik0@gmail.com>
2026-06-20 22:28:49 +0200

    Nova - Multi Copies & Mr. Mega Synergies

--- scripts/items/nova.lua
+++ scripts/items/nova.lua
@@ -1,9 +1,9 @@
-local TEAR_SCALE_BONUS = -0.5
+local TEAR_SCALE_BONUS = -0.30
 local EXPLOSION_BASE_DAMAGE = 20
 local EXPLOSION_DAMAGE_MULTIPLIER = 10
+local TEARS_MULTIPLIER = 0.42
 
 local firingFormation = false
-local firstEnemyKilled = true
 local lastHitDamage = {}
 
 local function ChangeTearColor(_, player, cacheFlag)
@@ -13,6 +13,24 @@ local function ChangeTearColor(_, player, cacheFlag)
             0.1, 0.1, 0.3, 0.1)
     end
 
+end
+
+local function ShootTears(_, player, tear, doubleNova)
+    local vel = tear.Velocity
+    local pos = tear.Position
+    local dir = vel:Normalized()
+    local directions = {} 
+    if doubleNova then
+        directions = {Vector(0, 0), dir, -dir, dir:Rotated(60), dir:Rotated(-60), dir:Rotated(120), dir:Rotated(-120)}
+    else
+        directions = {dir, -dir, dir:Rotated(90), dir:Rotated(-90)}
+    end
+
+    local spread = (tear.Scale + 1) * 4 -- distance from center point to each tear in the formation
+    
+    for _, d in ipairs(directions) do
+        player:FireTear(pos + d * spread, vel, false, true)
+    end
 end
 
 local function PostFireTear(_, tear)
@@ -25,29 +43,26 @@ local function PostFireTear(_, tear)
 
     if firingFormation then return end   
 
-    local vel = tear.Velocity
-    local pos = tear.Position
-    local dir = vel:Normalized()
-    local perp = Vector(-dir.Y, dir.X) -- 90° rotation of the direction
-
-    local spread = (tear.Scale + 1) * 4 -- distance from center point to each tear in the formation
     firingFormation = true
-
-    player:FireTear(pos + dir * spread, vel, false, true) -- front
-    player:FireTear(pos - dir * spread, vel, false, true) -- back
-    player:FireTear(pos + perp * spread, vel, false, true) -- left
-    player:FireTear(pos - perp * spread, vel, false, true) -- right
-
+    ShootTears(_, player, tear, player:GetCollectibleNum(NOVA_ID) > 1)
     firingFormation = false
 
-    tear:Remove() -- remove the original center tear so you end up with exactly 4
+    tear:Remove() -- remove the original tear
 end
 
-local function CalcRadiusMultiplier(range)
-    local radiusMultiplier = math.log(range/40 + 4, 10)
+local function CalcRadiusMultiplier(playerRange)
+    local radiusMultiplier = math.log(playerRange/40 + 4, 10)
     radiusMultiplier = math.max(radiusMultiplier, 0.9) -- Ensure the radius multiplier is at least 0.75
     radiusMultiplier = math.min(radiusMultiplier, 1.5) -- Ensure the radius multiplier does not exceed 3.0
     return radiusMultiplier
+end
+
+local function CalcExplosionDamage(playerDamage, hasMrMega)
+    local damgage = playerDamage * EXPLOSION_DAMAGE_MULTIPLIER + EXPLOSION_BASE_DAMAGE
+    if hasMrMega then
+        damgage = damgage * 1.85
+    end
+    return damgage
 end
 
 local function PostNpcDeath (_, 
@@ -56,12 +71,21 @@ local function PostNpcDeath (_,
     if not entityNpc:IsEnemy() then return end
 
     if not lastHitDamage[entityNpc.InitSeed] then return end
+
+    ---@param player EntityPlayer
     local player = lastHitDamage[entityNpc.InitSeed]
     if not player:HasCollectible(NOVA_ID) then return end
+    local data = player:GetData()
 
-    if firstEnemyKilled then
+    if not data.nbNovaTrigged then
+        data.nbNovaTrigged = 0
+    end
 
-        firstEnemyKilled = false
+    print("Player " .. player:GetName() .. " has triggered Nova " .. data.nbNovaTrigged .. " times.")
+    print("And has "  .. player:GetCollectibleNum(NOVA_ID) .. " Nova items.")
+    if data.nbNovaTrigged < player:GetCollectibleNum(NOVA_ID) then
+        data.nbNovaTrigged = data.nbNovaTrigged + 1
+
         local blackHole = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.BLACK_HOLE, 0, entityNpc.Position, Vector(0, 0), player)
     blackHole:SetColor(Color(0.1, 0.1, 0.1, 0.9, 
         0, 0, 0, 
@@ -71,10 +95,10 @@ local function PostNpcDeath (_,
         
         blackHole:GetData().IsCustomGravityWell = true
         blackHole:GetData().Timer = 60 
-        blackHole:GetData().Damage = player.Damage * EXPLOSION_DAMAGE_MULTIPLIER + EXPLOSION_BASE_DAMAGE
+        blackHole:GetData().Damage = CalcExplosionDamage(player.Damage, player:HasCollectible(CollectibleType.COLLECTIBLE_MR_MEGA))
         blackHole:GetData().Position = entityNpc.Position
         blackHole:GetData().RadiusMultiplier = CalcRadiusMultiplier(player.TearRange)
-        print("Black hole radiusMultiplier: " .. tostring(blackHole:GetData().RadiusMultiplier))
+        --print("Black hole radiusMultiplier: " .. tostring(blackHole:GetData().RadiusMultiplier))
     end 
 end
 
@@ -109,18 +133,16 @@ local function OnEffectUpdate(_, effect)
     end
 end
 
-Mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, OnEffectUpdate)
-
 local function PostNewRoom()
-    firstEnemyKilled = true
+    --- resets the data of all players
+    for i=0, Game():GetNumPlayers() -1 do
+        local player = Isaac.GetPlayer(i)
+        local data = player:GetData()
+        data.nbNovaTrigged = 0
+    end
+
     lastHitDamage = {}
 end
-
-Mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, PostNpcDeath)
-Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, PostNewRoom)
-Mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, PostFireTear)
-Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, ChangeTearColor, CacheFlag.CACHE_TEARCOLOR)
-
 
 local function EntityTakeDamage(_, 
     entity, ---@param entity Entity
@@ -142,4 +164,20 @@ local function EntityTakeDamage(_,
     end
 end
 
+local function EvaluateFireDelay(_, 
+    player,     ---@param player EntityPlayer
+    cacheFlag   ---@param cacheFlag CacheFlag
+)
+    if cacheFlag == CacheFlag.CACHE_FIREDELAY and player:HasCollectible(NOVA_ID) then
+        print(player.MaxFireDelay)
+        player.MaxFireDelay = Mod:toMaxFireDelay(Mod:toTearsPerSecond(player.MaxFireDelay) * TEARS_MULTIPLIER)
+    end
+end
+
+Mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, PostNpcDeath)
+Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, PostNewRoom)
+Mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, PostFireTear)
+Mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, OnEffectUpdate)
+Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, ChangeTearColor, CacheFlag.CACHE_TEARCOLOR)
 Mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, EntityTakeDamage)
+Mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, EvaluateFireDelay, CacheFlag.CACHE_FIREDELAY)
