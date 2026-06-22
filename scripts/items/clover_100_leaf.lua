5f37d03ebeef090980e9f04f6ce3b34051a0aa07
leokoko42 <leokocik0@gmail.com>
2026-06-23 00:23:01 +0200

    100-Leaf Clover - Stackable Planetarium & Devil chances

--- scripts/items/clover_100_leaf.lua
+++ scripts/items/clover_100_leaf.lua
@@ -3,15 +3,17 @@ local DEVIL_CHANCE = 20.0
 local LUCK_MULTIPLIER = 1.5
 
 local function addPlanetariumChance(_, chance)
-    if PlayerManager.AnyoneHasCollectible(CLOVER_100_LEAF_ID) then
-        return chance + PLANETARIUM_CHANCE/100
+    for _, player in pairs(PlayerManager.GetPlayers()) do ---@param player EntityPlayer
+        chance = chance + player:GetCollectibleNum(CLOVER_100_LEAF_ID) * PLANETARIUM_CHANCE/100 
     end
+    return chance
 end
 
 local function addDevilChance(_, chance)
-    if PlayerManager.AnyoneHasCollectible(CLOVER_100_LEAF_ID) then
-        return chance + DEVIL_CHANCE/100
+    for _, player in pairs(PlayerManager.GetPlayers()) do ---@param player EntityPlayer
+        chance = chance + player:GetCollectibleNum(CLOVER_100_LEAF_ID) * DEVIL_CHANCE/100 
     end
+    return chance
 end
 
 local function addLuckMult(_, 
